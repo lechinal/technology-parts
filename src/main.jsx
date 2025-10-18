89e96da6ea2c110ad8014123f365c23175426b45
lechinal <alin.lechintan@outlook.com>
2025-10-18 18:47:30 +0300

    favicons added

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,26 @@ import "./App.css";
 
 import App from "./App.jsx";
 
+// IMPORT FAVICONS DIN SRC
+// -----------------------
+import iPhoneFavicon from "./assets/favicons/iPhone.png";
+import AndroidFavicon from "./assets/favicons/Android.png";
+import BrowserFavicon from "./assets/favicons/browser.png";
+
+// FUNCTIE PENTRU SETAREA FAVICONULUI
+const setFavicon = (href, rel = "icon", sizes) => {
+  const link = document.createElement("link");
+  link.rel = rel;
+  if (sizes) link.sizes = sizes;
+  link.href = href;
+  document.head.appendChild(link);
+};
+
+// ADAUGA FAVICONS IN HEAD
+setFavicon(iPhoneFavicon, "apple-touch-icon", "180x180");
+setFavicon(AndroidFavicon, "icon", "196x196");
+setFavicon(BrowserFavicon, "icon", "32x32");
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
